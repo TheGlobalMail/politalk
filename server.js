17f619fcbecf794b8856cf822edc5f9f27e74670
Andrew Cobby <andrew.cobby@gmail.com>
2012-11-12 10:37:20 +1100

    Dump server running

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var static = require('node-static'),
     http   = require('http');
 
 var file = new (static.Server)('./dist');
+var port = process.env.PORT || 8080;
 
 http.createServer(function(request, response) {
     request.addListener('end', function() {
         file.serve(request, response);
     });
-}).listen(process.env.PORT || 8080);
+}).listen(port);
+console.log('App server running on port: ' + port);
