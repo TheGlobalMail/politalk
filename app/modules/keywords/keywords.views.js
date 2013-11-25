c27a748c30e9c56c18ba50dd0f51266a829e70c5
Nikhil <nikhil.sonnad@gmail.com>
2013-11-25 14:05:31 +1100

    line break in person title/date

--- app/modules/keywords/keywords.views.js
+++ app/modules/keywords/keywords.views.js
@@ -187,8 +187,9 @@ PolitalkApp.module('Keywords.Views', function(Views, App) {
             if (this.currentSpeaker){
               var speakerImage = this.currentSpeaker.get('image') 
               this.imageUrl = speakerImage ? '/modules/members/members-img/mpsL/' + this.currentSpeaker.get('person_id') + '.jpg' : '/modules/members/img/avatar.png'
-              this.currentSpeaker = this.currentSpeaker.get('first_name') + ' ' + this.currentSpeaker.get('last_name') +
-                ' ' + this.currentSpeaker.get('roleAndTenure');
+              this.currentSpeakerName = this.currentSpeaker.get('first_name') + ' ' + this.currentSpeaker.get('last_name');
+              this.currentSpeakerRole = this.currentSpeaker.get('roleAndTenure');
+              this.currentSpeaker = this.currentSpeakerName + ' ' + this.currentSpeakerRole;
             }
             var entity = this.currentSpeaker || this.currentParty;
             var html = '';
@@ -214,8 +215,7 @@ PolitalkApp.module('Keywords.Views', function(Views, App) {
             if (!this.currentSpeaker) {
                 $('.member-info-container').html(thumbnailHtml).removeClass('open');
             }else{
-                // console.log(this)
-                var thumbnailHtml = '<div class="member-thumb"><img alt="' + this.currentSpeaker + '" src="' + this.imageUrl + '" /></div><div class="member-name"><p>' + this.currentSpeaker + '</p></div>';
+                var thumbnailHtml = '<div class="member-thumb"><img alt="' + this.currentSpeaker + '" src="' + this.imageUrl + '" /></div><div class="member-name"><p>' + this.currentSpeakerName + '</br>' + this.currentSpeakerRole + '</p></div>';
                 $('.member-info-container').html(thumbnailHtml).addClass('open');
             };
         },
